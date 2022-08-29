62f843f5961a76fb96d1f867f02ed0fe8ca7df77
netbenix <netbenix@codenoodles.de>
2022-08-29 15:25:51 +0200

    Reworked RenderObject class

--- src/main/kotlin/RenderObject.kt
+++ src/main/kotlin/RenderObject.kt
@@ -2,6 +2,8 @@ import org.openrndr.draw.*
 import org.openrndr.math.Vector3
 import org.openrndr.math.Vector4
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.Vector
 
 class RenderObject(val vertCount: Int, val indexCount: Int) {
     val vertBuff: VertexBuffer = vertexBuffer(vertexFormat {
@@ -9,19 +11,32 @@ class RenderObject(val vertCount: Int, val indexCount: Int) {
         color(4)
     }, vertCount)
     val indexBuff: IndexBuffer = indexBuffer(indexCount, IndexType.INT16)
-
     var shader: ShadeStyle = shadeStyle {}
     var position: Vector3 = Vector3(0.0, 0.0, 0.0)
+    var rotation: Vector3 = Vector3(0.0, 0.0, 0.0)
 
 
     fun fillIndexBuff(byteBuff: ByteBuffer) {
         indexBuff.write(byteBuff)
     }
+
     fun insertVert(offset: Int, pos: Vector3, color: Vector4){
         vertBuff.put(offset) {
             write(pos)
             write(color)
         }
+    }
+
+    fun insertIndexes(indexArr: ShortArray){
+        val bb = ByteBuffer.allocateDirect(indexCount*2)
+        bb.order(ByteOrder.nativeOrder())
+
+        for (i in 0 until indexCount){
+            bb.putShort(indexArr[i])
+        }
+
+        bb.rewind()
+        fillIndexBuff(bb)
     }
 
 }
