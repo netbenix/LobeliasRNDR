28976af74b7bf38408ec02aecc34087369da5a55
netbenix <netbenix@codenoodles.de>
2022-08-31 16:04:01 +0200

    Put renderObject Method into seperate file

---
+++ src/main/kotlin/RenderMeths.kt
@@ -0,0 +1,21 @@
+import org.openrndr.Program
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.RenderTarget
+import org.openrndr.draw.isolatedWithTarget
+import org.openrndr.extra.camera.OrbitalCamera
+
+//TODO: Find nice place for this
+fun renderObjects(rt: RenderTarget, program: Program, camera: OrbitalCamera, objects: List<RenderObject>){
+    with(program) {
+        drawer.isolatedWithTarget(rt) {
+            drawer.clear(ColorRGBa.CYAN) //SkyColor (NECESSARY)
+            camera.beforeDraw(this, program)
+            objects.forEach {
+                with(it) {
+                    render(this@isolatedWithTarget)
+                }
+            }
+            camera.afterDraw(this, program)
+        }
+    }
+}
